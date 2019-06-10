fa9ad4cc4db4a75ee1a39d9b9a5057da7f10a9e1
Oliver Davies <oliver@oliverdavies.uk>
2019-06-10 20:49:24 +0100

    Re-add tailwindcss-transition plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,14 @@ module.exports = {
     }
   },
   variants: {},
+  plugins: [
+    require('tailwindcss-transition')({
+      standard: 'all .3s ease',
+      transitions: {
+        'box-shadow': 'box-shadow 0.25s ease-in-out',
+        'zoom': 'all 0.25s ease-in-out',
+      },
+      variants: ['responsive', 'hover'],
+    })
   ]
 }
